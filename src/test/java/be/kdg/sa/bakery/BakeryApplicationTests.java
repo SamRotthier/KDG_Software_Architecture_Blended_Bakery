0a0a47fdc5bb1449b8dfc356ee81ae01b6096fe8
Matthias Vermeiren <matthias.vermeiren@student.kdg.be>
2024-03-02 19:22:39 +0100

    Initial commit

---
+++ src/test/java/be/kdg/sa/bakery/BakeryApplicationTests.java
@@ -0,0 +1,13 @@
+package be.kdg.sa.bakery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

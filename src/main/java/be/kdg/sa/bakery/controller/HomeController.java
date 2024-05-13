1d7564926889945d81447bd54a67c97166f5f35c
Matthias Vermeiren <matthias.vermeiren@student.kdg.be>
2024-05-13 21:45:11 +0200

    index and navbar thymeleaf

---
+++ src/main/java/be/kdg/sa/bakery/controller/HomeController.java
@@ -0,0 +1,12 @@
+package be.kdg.sa.bakery.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+}
